01c9026e53d9c336779ec5f89c7cf484a7606862
JorgeQZ <jorge_qzg@hotmail.com>
2023-09-01 17:02:29 -0600

    resultados

--- js/thd.js
+++ js/thd.js
@@ -2,7 +2,6 @@
     "use strict";
     $(document).ready(function () {
         $('.burguer').on('click', function () {
-            console.log('click');
             $(this).toggleClass('active');
             $('.menu').slideToggle(500);
         });
